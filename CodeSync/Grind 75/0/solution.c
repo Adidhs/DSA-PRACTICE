0954722f4e0a755d6b28beec494fd2753a62e7a7
CodeSync <codesync@users.noreply.github.com>
2026-09-25 19:57:05 +0530

    Solved: 0 (LeetCode) Sheets: Blind75, Grind75, Grind169, NeetCode150, TopInterview150, Top100Liked, StriverA2Z, LoveBabbar450, StriverSDE

---
+++ CodeSync/Grind 75/0/solution.c
@@ -0,0 +1,22 @@
+/*
+ * Platform: LeetCode
+ * Problem: 0
+ * URL: https://leetcode.com/problems/3sum/description/
+ * Language: C
+ * Difficulty: Medium
+ * Topics: Array, Two Pointers, Sorting
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-25T14:26:57.417Z
+ */
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume 
+ caller calls free().
+ */
+int** threeSum(int* nums, int numsSize, int* returnSize, int** 
+returnColumnSizes) {
+    
+}
